7b36805f80fcc60093bca037b3151b93cebb32e8
jluo <jluo8@wgu.edu>
2024-02-06 18:59:07 -0800

    update

--- app/layout.js
+++ app/layout.js
@@ -12,12 +12,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
  return (
-  <ClerkProvider>
-   <html lang='en'>
-    <body className={inter.className}>
-     <Providers>{children}</Providers>
-    </body>
-   </html>
-  </ClerkProvider>
+  <>
+   <head>
+    <link
+     rel='icon'
+     href='data:image/svg+xml,
+    <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+    <text y=%22.9em%22 font-size=%2290%22>🎆</text></svg>'></link>
+   </head>
+
+   <ClerkProvider>
+    <html lang='en'>
+     <body className={inter.className}>
+      <Providers>{children}</Providers>
+     </body>
+    </html>
+   </ClerkProvider>
+  </>
  );
 }
--- components/SidebarHeader.jsx
+++ components/SidebarHeader.jsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import ThemeToggle from "./ThemeToggle";
 import { BsChatText } from "react-icons/bs";
 const SidebarHeader = () => {
  return (
   <div className='flex items-center mb-4 gap-4 px-4 '>
-   <BsChatText className='w-10 h-10 text-primary-500' />
+   {/* <BsChatText className='w-10 h-10 text-primary-500' /> */}
+   <Image
+    src='/cat10.png'
+    width={50}
+    height={50}
+    className='w-10 h-10 text-primary-500'
+   />
 
    <h2 className='text-xl font-extrabold text-primary mr-auto '>
     PurpleCat GPT
